315d58e78e978bd231e640479f567c1062d7ad7a
Ben Davies <ben.davies@gmail.com>
2017-08-31 10:21:12 +0100

    add support for converting to null

--- tests/ValueConverter/MappingValueConverterTest.php
+++ tests/ValueConverter/MappingValueConverterTest.php
@@ -1,21 +1,30 @@
 <?php
+
 namespace Port\Tests\ValueConverter;
 
 use Port\ValueConverter\MappingValueConverter;
 
 class MappingValueConverterTest extends \PHPUnit_Framework_TestCase
 {
+    public function testConvert()
+    {
+        $converter = new MappingValueConverter([
+            'source' => 'destination',
+            'foo' => null,
+        ]);
+
+        $this->assertSame('destination', call_user_func($converter, 'source'));
+        $this->assertNull(call_user_func($converter, 'foo'));
+    }
+
     /**
      * @expectedException Port\Exception\UnexpectedValueException
      * @expectedExceptionMessage Cannot find mapping for value "unexpected value"
      */
-    public function testConvert()
+    public function testExceptionIsThrownWhenValueIsNotFound()
     {
-        $converter = new MappingValueConverter(array(
-            'source' => 'destination'
-        ));
+        $converter = new MappingValueConverter([]);
 
-        $this->assertSame('destination', call_user_func($converter, 'source'));
         call_user_func($converter, 'unexpected value');
     }
 }
